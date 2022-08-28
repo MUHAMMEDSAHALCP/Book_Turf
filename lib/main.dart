9619132473e9108e0df16ee366e6a30e73255645
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-08-28 11:08:43 +0530

    Successfully implemented MVVM design pattern

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:book_turf/app/splash/view/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashView(),
+    );
+  }
+}
