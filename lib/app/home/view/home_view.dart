71d23d5e29ad5970ff73d926c6b80bccda40bb2e
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-09-05 08:21:11 +0530

    half ui completed the rest tommorow

--- lib/app/home/view/home_view.dart
+++ lib/app/home/view/home_view.dart
@@ -1,20 +1,111 @@
+import 'package:book_turf/app/utilities/colors.dart';
+import 'package:book_turf/app/utilities/styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class  HomeView extends StatelessWidget {
+class HomeView extends StatelessWidget {
   static String id = "Home_view";
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
-            "HomeView View   ",
-            style: TextStyle(fontSize: 20),
-          ),
-        ],
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.location_on,
+                    color: blackColor,
+                  ),
+                  label: Text(
+                    "Thottummal, Thalassery...",
+                    style: textStyle.copyWith(
+                      color: blackColor,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.notifications),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  CupertinoSearchTextField(
+                    decoration: BoxDecoration(
+                      color: cardColor,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  height20,
+                  Container(
+                    height: size.height * .16,
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15, top: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Hi, Muhammed...",
+                                style: textStyle.copyWith(
+                                  color: whiteColor,
+                                ),
+                              ),
+                              height10,
+                              Text(
+                                "Explore vanues and \nbook your favourite spot.",
+                                style: textStyle.copyWith(fontSize: 12),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.arrow_forward,
+                                  color: whiteColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Container(
+                            height: size.height * .3,
+                            width: size.width * .3,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              image: const DecorationImage(
+                                  image: AssetImage(
+                                    "assets/images/messi.png",
+                                  ),
+                                  fit: BoxFit.fill),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
