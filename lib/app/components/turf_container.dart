e49be16b9d72f41cab8fe5aee1e7fbef6050958e
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-10-08 17:09:04 +0530

    modified

--- lib/app/components/turf_container.dart
+++ lib/app/components/turf_container.dart
@@ -5,58 +5,72 @@ import 'package:flutter/material.dart';
 
 class TurfContainer extends StatelessWidget {
   final String turfName;
-
+  final String turfPlace;
+  final String turfImage;
   final bool visible;
-  const TurfContainer({Key? key, required this.visible, required this.turfName})
-      : super(key: key);
+  final Function() bookOnclick;
+  final Function() cardOnTap;
+
+  const TurfContainer({
+    Key? key,
+    required this.visible,
+    required this.turfName,
+    required this.turfPlace,
+    required this.turfImage,
+    required this.cardOnTap,
+    required this.bookOnclick,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: boxDecoration.copyWith(
-        color: cardColor,
-        borderRadius: const BorderRadius.only(
-          topRight: Radius.circular(100),
-          topLeft: Radius.circular(100),
+    return GestureDetector(
+      onTap: cardOnTap,
+      child: Container(
+        decoration: boxDecoration.copyWith(
+          color: cardColor,
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(100),
+            topLeft: Radius.circular(100),
+            bottomRight: Radius.circular(50),
+            bottomLeft: Radius.circular(0),
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            height10,
-            const CircleAvatar(
-              radius: 70,
-              backgroundImage: AssetImage("assets/images/jawans.png"),
-            ),
-            Text(
-              turfName,
-              textAlign: TextAlign.center,
-              style: textStyle.copyWith(fontSize: 16),
-            ),
-            height5,
-            Text(
-              "Kinavakkal Kut...",
-              style: textStyle.copyWith(fontSize: 14, color: greyColor),
-            ),
-            height5,
-            Text(
-              "5km",
-              style: textStyle.copyWith(fontSize: 14, color: greyColor),
-            ),
-            height10,
-            Visibility(
-              visible: visible,
-              child: MaterialButtonWidget(
-                height: 35,
-                text: "Book",
-                color: secondaryColor,
-                textColor: whiteColor,
-                onClick: () {},
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: NetworkImage(turfImage),
               ),
-            ),
-          ],
+              height10,
+              Text(
+                turfName,
+                textAlign: TextAlign.center,
+                style: textStyle.copyWith(fontSize: 16),
+                maxLines: 1,
+              ),
+              height5,
+              Text(
+                turfPlace,
+                style: textStyle.copyWith(fontSize: 14, color: greyColor),
+                maxLines: 1,
+              ),
+              height5,
+              height10,
+              Visibility(
+                visible: visible,
+                child: MaterialButtonWidget(
+                  height: 35,
+                  text: "Book",
+                  color: secondaryColor,
+                  textColor: whiteColor,
+                  onClick: bookOnclick,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
