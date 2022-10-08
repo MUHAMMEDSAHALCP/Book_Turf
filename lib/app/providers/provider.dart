ff55f2f91e810b325d0aadfa3c594b51e0af2f98
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-10-08 17:17:09 +0530

    new providers added

--- lib/app/providers/provider.dart
+++ lib/app/providers/provider.dart
@@ -1,9 +1,14 @@
 import 'package:book_turf/app/modules/bottom_navigation/view_model/bottom_nav_provider.dart';
 import 'package:book_turf/app/modules/email_otp/view_model/otp_view_model.dart';
+import 'package:book_turf/app/modules/home/services/location_api.dart';
 import 'package:book_turf/app/modules/home/view_model/home_view_model.dart';
+import 'package:book_turf/app/modules/phone_otp/view_model/phone_otp_view_model.dart';
+import 'package:book_turf/app/modules/profile/view_model/profile_view_model.dart';
 import 'package:book_turf/app/modules/sign_in/view_model/signin_view_model.dart';
 import 'package:book_turf/app/modules/sign_up/service/services/signup_api_service.dart';
 import 'package:book_turf/app/modules/sign_up/view_model/signup_view_model.dart';
+import 'package:book_turf/app/modules/signin_phone/view_model/phone_view_model.dart';
+import 'package:book_turf/app/modules/spot/view_model/spot_view_model.dart';
 import 'package:book_turf/main.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -14,6 +19,12 @@ class Providers {
       providers: [
         ChangeNotifierProvider(
           create: (context) => BottomNavProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => PhoneViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => PhoneOtpViewModel(),
         ),
         ChangeNotifierProvider(
           create: (context) => HomeViewModel(),
@@ -29,6 +40,15 @@ class Providers {
         ),
         ChangeNotifierProvider(
           create: (context) => SignupApiService(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => SpotViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProfileViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => GetUserLoction(),
         ),
       ],
       child: const MyApp(),
