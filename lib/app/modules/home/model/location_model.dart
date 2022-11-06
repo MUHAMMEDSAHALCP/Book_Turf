7576feaecb1789e93188af36751d5eb70591c494
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-11-06 11:28:47 +0530

    modified

--- lib/app/modules/home/model/location_model.dart
+++ lib/app/modules/home/model/location_model.dart
@@ -1,28 +1,44 @@
 class UserLocationModel {
   UserLocationModel({
+    this.message,
+    this.error,
     this.features,
-    this.type,
   });
-  String? type;
+  String? message;
+  bool? error;
   List<Feature>? features;
 
   factory UserLocationModel.fromJson(Map<String, dynamic> json) =>
       UserLocationModel(
         features: List<Feature>.from(
-          json["features"].map(
-            (dynamic x) => Feature.fromJson(x),
-          ),
-        ),
-        type: json["type"],
+            json["features"].map((x) => Feature.fromJson(x))),
       );
 }
 
 class Feature {
   Feature({
     this.placeName,
+    this.context,
   });
+
   String? placeName;
+  List<Context>? context;
+
   factory Feature.fromJson(Map<String, dynamic> json) => Feature(
         placeName: json["place_name"],
+        context:
+            List<Context>.from(json["context"].map((x) => Context.fromJson(x))),
+      );
+}
+
+class Context {
+  Context({
+    this.text,
+  });
+
+  String? text;
+
+  factory Context.fromJson(Map<String, dynamic> json) => Context(
+        text: json["text"],
       );
 }
