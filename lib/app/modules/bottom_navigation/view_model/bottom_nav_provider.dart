6535b81af68cb4ee80597c393a5fa4f00d580538
MUHAMMED SAHAL CP <muhammedsahalshah@gmail.com>
2022-11-09 16:01:44 +0530

    modified

--- lib/app/modules/bottom_navigation/view_model/bottom_nav_provider.dart
+++ lib/app/modules/bottom_navigation/view_model/bottom_nav_provider.dart
@@ -4,15 +4,14 @@ import 'package:book_turf/app/modules/spot/view/spot_view.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavProvider extends ChangeNotifier {
-  int _currentIndex = 1;
-  get currentIndex => _currentIndex;
+  int currentIndex = 1;
   final screens = [
     const SpotView(),
     const HomeView(),
     const ProfileView(),
   ];
-  set currentIndex(index) {
-    _currentIndex = index;
+  currentIndexState(index) {
+    currentIndex = index;
     notifyListeners();
   }
 }
